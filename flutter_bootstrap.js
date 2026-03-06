ab5156d9599f70e8b40166513cda26359d6789e6
Akshay Laghate <akki.the.abe@gmail.com>
2026-03-06 18:42:14 +0530

    + alignment fix & others fixes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3239950466"
+    serviceWorkerVersion: "2685538732"
   }
 });
