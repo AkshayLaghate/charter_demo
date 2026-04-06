3de7424b3e6e63f7c2130c85b4805a81aae11ea8
Akshay Laghate <akki.the.abe@gmail.com>
2026-04-07 00:25:44 +0530

    zoho

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1944112367" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1360210688" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
