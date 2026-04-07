e019ae891b51d41636f178954f8016bbc462b991
Akshay Laghate <akki.the.abe@gmail.com>
2026-04-07 21:58:51 +0530

    + Search Functionality

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "605652155" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4045030953" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
